eea04946dea9cbbd6b3393950d5850db3f7e2bce
usuario <usuario@ERNI-PC>
2022-09-13 17:33:49 -0700

    Agregar archivos de proyecto.

---
+++ Practica2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Practica2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int Edad = 21;
+            String Nombre = "Jose Ernesto Leon Uribe";
+            String Pais = "Italia";
+
+                Console.WriteLine("Holis, mi nombre es: {1}, tengo {0} años de edad y  me encantaria viajar a {2} :D", Edad, Nombre, Pais);
+            Console.ReadKey();
+        }
+    }
+}
